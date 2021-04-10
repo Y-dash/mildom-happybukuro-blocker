962f199513609f810d8613a6e2fbb41afe866b86
Y-dash <y-dash@y-dash.net>
2021-04-11 02:15:38 +0900

    突貫

---
+++ src/scripts/content.ts
@@ -0,0 +1,23 @@
+const COMMON_CLASS_NAME: string = 'ts--mildom-happybukuro-blocker--checked';
+const HAPPYBUKURO_MESSAGE: string = 'ハッピー袋ありがとう！';
+
+let rootElement: HTMLElement | null = document.getElementById('root');
+
+let mutationObserver: MutationObserver = new MutationObserver(() => {
+	document.querySelectorAll(`div.message-list > div:not(.${COMMON_CLASS_NAME})`).forEach((element: Element) =>  {
+		element.classList.add(COMMON_CLASS_NAME);
+
+		let comment = element.querySelector('.sc-173ztwo-0')?.textContent;
+		
+		if (comment && comment.indexOf(HAPPYBUKURO_MESSAGE) != -1) {
+			element.setAttribute('style', 'display: none;');
+		}
+	});
+});
+
+if (rootElement) {
+	mutationObserver.observe(rootElement, {
+		childList: true,
+		subtree: true
+	});
+}
